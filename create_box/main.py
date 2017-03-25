bbf32e3ac451f59c22f8bd33792f3b5602257c54
Alejos Alex Bran <alexabran13@gmail.com>
2017-03-25 16:46:32 +0000

    Ready for pull

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,26 @@
 """This is the entry point of the program."""
 
 
-def create_box(height, width, character):
-    pass
-
+def create_box(height, width, char):
+    result = ''
+    if height >= 1 and width >= 1:
+        for i in range(height):
+            result += char * width + '\n'
+        return result
+    return "invalid input"
+    
+def create_hollow_box(height, width, character):
+    box = character * width
+    top_bot = str(box) + '\n'
+    if height >= 1 and width >= 1:
+        for i in range(width):
+            middle = character+((width - 2)*' ')+character+'\n'
+        return top_bot+(middle * (height - 2))+top_bot
+    return "invalid input"
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+    
+if __name__ == '__main__':
+    create_hollow_box(4, 5, '*')
+    
--- tests/test_main.py
+++ tests/test_main.py
@@ -26,4 +26,41 @@ class TestCreateBox(unittest.TestCase):
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
 
+    def test_third_box(self):
+        self.assertEqual(create_box(3, 24, 'x'), third_box_expected)
+        
     # Add your own test using third_box_expected
+
+from create_box import create_hollow_box
+
+fourth_box_expected = """
+*****
+*   *
+*   *
+*****
+""".lstrip()
+
+fifth_box_expected = """
+@@@@@@
+@    @
+@    @
+@@@@@@
+""".lstrip()
+
+sixth_box_expected = """
+xxxxxxxxxxxxxxxxxxxxxxxx
+x                      x
+xxxxxxxxxxxxxxxxxxxxxxxx
+""".lstrip()
+
+
+class TestCreateHollowBox(unittest.TestCase):
+    def test_box(self):
+        self.assertEqual(create_hollow_box(4, 5, '*'), fourth_box_expected)
+
+    def test_small_box(self):
+        self.assertEqual(create_hollow_box(4, 6, '@'), fifth_box_expected)
+
+    def test_third_box(self):
+        self.assertEqual(create_hollow_box(3, 24, 'x'), sixth_box_expected)
+        
